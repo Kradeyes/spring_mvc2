f564dbb9b19be454c528509bc15d61ca8b929b0f
Kradeyes <yanin.r.a@mail.ru>
2021-06-08 14:02:44 +0300

    Отображение всех работников

---
+++ src/main/java/com/romanyanin/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,8 @@
+package com.romanyanin.spring.mvc_hibernate_aop.dao;
+
+import com.romanyanin.spring.mvc_hibernate_aop.entity.Employee;
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployees();
+}
